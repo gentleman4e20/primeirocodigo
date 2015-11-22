66039bc9b9c7d89f3e633ef00a2e7a78e940a79d
Thiago Rocha Alves <tralves@outlook.com>
2015-11-22 21:18:15 -0200

    Update meucodigo.c

--- meucodigo.c
+++ meucodigo.c
@@ -2,20 +2,21 @@
   
 int main() {
   
-   int n[1000];
-    int t,x,c;
-x=0;
- 
-scanf("%d",&t);
- 
-if(t>=2 && t<=50){
+int n[1000];
+int t,c,x=0;
 
-for(c=0;c<1000;c++){
-  n[c]=x;
-    x++;
-  if(x==t){
-  x=0;}}
- 
+scanf("%d",&t);
+//a condição para o programa rodar N>=1 && N<=50
+if(t>=2 && t<=50){
+//sempre que o valor de x == t ele recebe 0 novamente 
+  for(c=0;c<1000;c++){
+    n[c]=x;
+      x++;
+      if(x==t){
+        x=0;}
+  }
+
+//parte final apresenta todos os numeros gerados através do contador acima
 for(c=0;c<1000;c++){
     printf("N[%d] = %d\n",c,n[c]);}}
      
